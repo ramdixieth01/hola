3dae41bd89ca72da60b6ba767d48e855f0ff3d81
Ram Narayan Dixieth <suvram.01@gmail.com>
2020-01-04 20:35:44 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HeaderNavbarComponent
   ],
   imports: [
     BrowserModule,
